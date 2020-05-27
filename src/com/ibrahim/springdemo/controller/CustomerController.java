63efa77671a1db39ae8418f499c5b1523f458410
Ibrahim Jimoh <ib.omogbolahan@yahoo.com>
2020-05-27 10:04:55 +0100

    Created a Service Layer which interacts directly with the controller.

--- src/com/ibrahim/springdemo/controller/CustomerController.java
+++ src/com/ibrahim/springdemo/controller/CustomerController.java
@@ -2,9 +2,11 @@ package com.ibrahim.springdemo.controller;
 
 import com.ibrahim.springdemo.dao.CustomerDAO;
 import com.ibrahim.springdemo.entity.Customer;
+import com.ibrahim.springdemo.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -15,13 +17,13 @@ public class CustomerController {
 
     // need to inject the customer dao
     @Autowired
-    private CustomerDAO customerDAO;
+    private CustomerService customerService;
 
-    @RequestMapping("/list")
+    @GetMapping("/list")
     public String listCustomers(Model theModel) {
 
         // get customers from the DAO
-        List<Customer> theCustomers = customerDAO.getCustomers();
+        List<Customer> theCustomers = customerService.getCustomers();
 
         // add the customers to the model
         theModel.addAttribute("customers", theCustomers);
---
+++ src/com/ibrahim/springdemo/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.ibrahim.springdemo.service;
+
+import com.ibrahim.springdemo.entity.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+
+    public List<Customer> getCustomers();
+
+}
